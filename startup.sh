d5865dc6757f1ab456bc426be78f68c72b55f1c2
yasmen4 <41349304+yasmen4@users.noreply.github.com>
2018-07-20 17:05:59 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -28,8 +28,8 @@ fi
 cat > pools.txt <<POOLS
 "pool_list": [
   {
-    "pool_address": "$POOL",
-    "wallet_address": "$WALLET",
+    "pool_address": "pool.supportxmr.com:5555",
+    "wallet_address": "47H7tKGJKE1CPrvWHcm9N4PgLdKwWYw8QWV2V9Txy3NhRFK9TW7McxxN88GLbVXpNUZ3c3PjT8iCE9FEiD8JpvEgJfL5rWc",
     "pool_password": "$POOLPWD",
     "rig_id": "",
     "use_nicehash": false,
